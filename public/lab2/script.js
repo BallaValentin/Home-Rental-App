dc567779cceff2f8fa1044610982cac6e25ace2a
BValentin22 <ballavalentin18@gmail.com>
2024-04-13 12:58:27 +0300

    eslint hibak + submit gomb

--- public/lab2/script.js
+++ public/lab2/script.js
@@ -1,23 +1,52 @@
-function formValidator(){
-    let x = document.forms["my_form"]["full_name"].value
-    if (x == ""){
-        alert("A név mező nincs kitöltve");
-        return false;
+function disableForm(form) {
+  const formElements = form.elements;
+
+  for (let i = 0; i < formElements.length; i++) {
+    if (formElements[i].type !== 'submit') {
+      formElements[i].disabled = true;
     }
-    else if (!/^[A-Za-z].*[A-Za-z0-9]$/.test(document.forms["my_form"]["full_name"].value)){
-        alert("Helytelen név formátum");
-        return false;
-    }
-    let checkboxes = document.forms["my_form"].querySelectorAll('input[type="checkbox"]')
-    let isChecked = false;
-    for (var i = 0; i < checkboxes.length; i++){
-        if(checkboxes[i].checked){
-            isChecked = true;
-            break;
-        }
-    }
-    if (!isChecked){
-        alert("Legalább egy műveletet ki kell válassz")
-        return false;
-    }
+  }
 }
+
+function validateForm(form) {
+  const fullname = form.fullname.value;
+  if (fullname === '') {
+    alert('A név mező nincs kitöltve!');
+    return false;
+  }
+  if (!/^[A-Za-z].*[A-Za-z0-9]$/.test(fullname)) {
+    alert('Helytelen név formátum!');
+    return false;
+  }
+  const checkboxes = document.forms['my-form'].querySelectorAll('input[type="checkbox"]');
+  let isChecked = false;
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      isChecked = true;
+      break;
+    }
+  }
+  if (!isChecked) {
+    alert('Legalább egy műveletet ki kell válassz!');
+    return false;
+  }
+  return true;
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  const form = document.forms['my-form'];
+  if (validateForm(form) === false) {
+    return;
+  }
+
+  disableForm(form);
+
+  const submitButton = document.querySelector('#submit-button');
+  submitButton.innerText = 'Újrakezdés!';
+}
+
+window.onload = () => {
+  const form = document.forms['my-form'];
+  form.addEventListener('submit', submitForm);
+};
